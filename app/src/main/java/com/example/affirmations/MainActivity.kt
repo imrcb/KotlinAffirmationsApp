74efeecd1423928750a2fce818d6098989fb31b4
Rahul Chandrababu <raahulrcb@gmail.com>
2021-06-21 10:38:19 +0530

    inital commit

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myDataset = Datasource().loadAffirmations()
+        binding.recyclerView.adapter = ItemAdapter(this, myDataset)
+        binding.recyclerView.setHasFixedSize(true)
+    }
+}
